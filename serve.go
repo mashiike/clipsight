249004f22a0f193b401918cae379914321e61df6
mashiike <ikeda-masashi@kayac.com>
2023-06-13 15:39:14 +0900

    check dashboard exists on serve

--- serve.go
+++ serve.go
@@ -107,6 +107,27 @@ func (app *ClipSight) RunServe(ctx context.Context, opt *ServeOption) error {
 			if !dashbord.IsVisible() {
 				continue
 			}
+			d, exists, err := app.DescribeDashboard(ctx, dashbord.DashboardID)
+			if err != nil {
+				slog.ErrorCtx(r.Context(), "failed describe dashboard",
+					slog.String("user_id", user.ID),
+					slog.String("error_code", "006"),
+					slog.String("detail", err.Error()),
+				)
+				w.WriteHeader(http.StatusInternalServerError)
+				json.NewEncoder(w).Encode(map[string]interface{}{
+					"code":  "006",
+					"error": "can not describe dashboard",
+				})
+				return
+			}
+			if !exists {
+				slog.WarnCtx(r.Context(), "dashboard not found",
+					slog.String("user_id", user.ID),
+					slog.String("dashboard_id", dashbord.DashboardID),
+				)
+				continue
+			}
 			output, err := qs.GenerateEmbedUrlForRegisteredUser(ctx, &quicksight.GenerateEmbedUrlForRegisteredUserInput{
 				AwsAccountId: aws.String(app.awsAccountID),
 				ExperienceConfiguration: &types.RegisteredUserEmbeddingExperienceConfiguration{
@@ -141,7 +162,7 @@ func (app *ClipSight) RunServe(ctx context.Context, opt *ServeOption) error {
 				return
 			}
 			result[fmt.Sprintf("dashbboard%d", i+1)] = map[string]interface{}{
-				"name": dashbord.Name,
+				"name": d.Name,
 				"url":  output.EmbedUrl,
 			}
 		}
